d6092c4232425048c2607f2e688339a7ec52698a
lixiang30 <534551900@qq.com>
2018-11-01 21:29:51 +0800

    采用xpath提取内容

--- ArticleSpider/ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/ArticleSpider/spiders/jobbole.py
@@ -1,11 +1,33 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
 
 
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
     allowed_domains = ['blog.jobbole.com']
-    start_urls = ['http://blog.jobbole.com/']
+    start_urls = ['http://blog.jobbole.com/114473/']
 
     def parse(self, response):
-        pass
+        title = response.xpath("//div[@class='entry-header']/h1/text()").extract()[0]
+        create_date = response.xpath("//p[@class='entry-meta-hide-on-mobile']/text()").extract()[0].strip().replace("·","").strip()
+        praise_nums = response.xpath("//span[contains(@class,'vote-post-up')]/h10/text()").extract()[0]
+        fav_nums = response.xpath("//span[contains(@class,'bookmark-btn')]/text()").extract()[0]
+        match_re = re.match(".*(\d+).*",fav_nums)
+        if match_re:
+            fav_nums = match_re.group(1)
+        else:
+            fav_nums = 0
+
+        comment_nums = response.xpath("//a[@href='#article-comment']/span/text()").extract()[0]
+        match_re = re.match(".*(\d+).*",comment_nums)
+        if match_re:
+            comment_nums = match_re.group(1)
+        else:
+            comment_nums = 0
+
+        content = response.xpath("//div[@class='entry']").extract()[0]
+
+        tag_list = response.xpath("//p[@class='entry-meta-hide-on-mobile']/a/text()").extract()
+        tag_list = [element for element in tag_list if not element.strip().endwith("评论")]
+        tags = ",".join(tag_list)
