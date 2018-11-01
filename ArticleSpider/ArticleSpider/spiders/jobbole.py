2730bd3754a4ff46ff5a0f4bca7ddcfb3561d7bc
lixiang30 <534551900@qq.com>
2018-11-01 22:14:33 +0800

    采用css方式获取数据

--- ArticleSpider/ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/ArticleSpider/spiders/jobbole.py
@@ -9,25 +9,50 @@ class JobboleSpider(scrapy.Spider):
     start_urls = ['http://blog.jobbole.com/114473/']
 
     def parse(self, response):
-        title = response.xpath("//div[@class='entry-header']/h1/text()").extract()[0]
-        create_date = response.xpath("//p[@class='entry-meta-hide-on-mobile']/text()").extract()[0].strip().replace("·","").strip()
-        praise_nums = response.xpath("//span[contains(@class,'vote-post-up')]/h10/text()").extract()[0]
-        fav_nums = response.xpath("//span[contains(@class,'bookmark-btn')]/text()").extract()[0]
-        match_re = re.match(".*(\d+).*",fav_nums)
+        # title = response.xpath("//div[@class='entry-header']/h1/text()").extract()[0]
+        # create_date = response.xpath("//p[@class='entry-meta-hide-on-mobile']/text()").extract()[0].strip().replace("·","").strip()
+        # praise_nums = response.xpath("//span[contains(@class,'vote-post-up')]/h10/text()").extract()[0]
+        # fav_nums = response.xpath("//span[contains(@class,'bookmark-btn')]/text()").extract()[0]
+        # match_re = re.match(".*(\d+).*",fav_nums)
+        # if match_re:
+        #     fav_nums = match_re.group(1)
+        # else:
+        #     fav_nums = 0
+        #
+        # comment_nums = response.xpath("//a[@href='#article-comment']/span/text()").extract()[0]
+        # match_re = re.match(".*(\d+).*",comment_nums)
+        # if match_re:
+        #     comment_nums = match_re.group(1)
+        # else:
+        #     comment_nums = 0
+        #
+        # content = response.xpath("//div[@class='entry']").extract()[0]
+        #
+        # tag_list = response.xpath("//p[@class='entry-meta-hide-on-mobile']/a/text()").extract()
+        # tag_list = [element for element in tag_list if not element.strip().endwith("评论")]
+        # tags = ",".join(tag_list)
+
+
+        # 采用css选择器方式
+        #extract()[0]如果为空，会报错．所以尽量使用extract_first(),后者没有的话会给个默认值
+        title = response.css(".entry-header h1::text").extract_first()
+        create_time = response.css("p.entry-meta-hide-on-mobile::text").extract_first().strip().replace("·","").strip()
+        praise_nums = response.css(".vote-post-up h10::text").extract_first()
+        fav_nums = response.css(".bookmark-btn::text").extract_first()
+        match_re = re.match(".*?(\d+).*", fav_nums)
         if match_re:
             fav_nums = match_re.group(1)
         else:
             fav_nums = 0
 
-        comment_nums = response.xpath("//a[@href='#article-comment']/span/text()").extract()[0]
-        match_re = re.match(".*(\d+).*",comment_nums)
+        comment_nums = response.css("a[href='#article-comment'] span::text").extract_first()
+        match_re = re.match(".*?(\d+).*", comment_nums)
         if match_re:
             comment_nums = match_re.group(1)
         else:
             comment_nums = 0
-
-        content = response.xpath("//div[@class='entry']").extract()[0]
-
-        tag_list = response.xpath("//p[@class='entry-meta-hide-on-mobile']/a/text()").extract()
-        tag_list = [element for element in tag_list if not element.strip().endwith("评论")]
+        content = response.css("div.entry").extract_first()
+        tag_list = response.css("p.entry-meta-hide-on-mobile a::text").extract()
+        tag_list = [element for element in tag_list if not element.strip().endswith("评论")]
         tags = ",".join(tag_list)
+        pass
