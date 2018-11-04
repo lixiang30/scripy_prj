3fcbe13c7185b958567941b211afb867fc65d9c7
lixiang30 <534551900@qq.com>
2018-11-04 23:55:58 +0800

    定义需要获取的字段

--- wxapp/wxapp/items.py
+++ wxapp/wxapp/items.py
@@ -9,6 +9,7 @@ import scrapy
 
 
 class WxappItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    author = scrapy.Field()
+    pub_time = scrapy.Field()
+    content = scrapy.Field()
