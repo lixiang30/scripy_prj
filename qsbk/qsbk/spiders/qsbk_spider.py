9299f974d4bd487039593504f164679076d243d0
lixiang30 <534551900@qq.com>
2018-11-04 20:40:29 +0800

    爬取多个页面内容

--- qsbk/qsbk/spiders/qsbk_spider.py
+++ qsbk/qsbk/spiders/qsbk_spider.py
@@ -8,12 +8,13 @@ class QsbkSpiderSpider(scrapy.Spider):
     name = 'qsbk_spider'   #　爬虫名字
     allowed_domains = ['qiushibaike.com']  #　限制爬取范围
     start_urls = ['https://www.qiushibaike.com/text/page/1/'] # 爬取开始的页面
+    base_domain = "https://www.qiushibaike.com"
 
     def parse(self, response):
         duanzidivs = response.xpath("//div[@id='content-left']/div")
         for duanzidiv in duanzidivs:
-            author = response.xpath(".//h2/text()").get().strip()
-            content = response.xpath("//div[@class='content']//text()").getall()  # getall()和extract()用法一样
+            author = duanzidiv.xpath(".//h2/text()").get().strip()
+            content = duanzidiv.xpath("//div[@class='content']//text()").getall()  # getall()和extract()用法一样
             content = "".join(content).strip()
 
             # 把数据传给pipelines
@@ -24,3 +25,8 @@ class QsbkSpiderSpider(scrapy.Spider):
             # 优化前面的代码，不用字典
             item = QsbkItem(author=author,content=content)
             yield item
+        next_url = response.xpath("//ul[@class='pagination']/li[last()]/a/@href").get()
+        if not next_url:
+            return
+        else:
+            yield scrapy.Request(self.base_domain+next_url,callback=self.parse)
