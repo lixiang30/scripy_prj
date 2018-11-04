d72f4ab053b5b3de9f708d5b88a873437c377e6a
lixiang30 <534551900@qq.com>
2018-11-04 23:57:06 +0800

    爬取需要的内容

--- wxapp/wxapp/spiders/wxapp_spider.py
+++ wxapp/wxapp/spiders/wxapp_spider.py
@@ -2,20 +2,25 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
-
+from wxapp.items import WxappItem
 
 class WxappSpiderSpider(CrawlSpider):
     name = 'wxapp_spider'
-    allowed_domains = ['http://www.wxapp-union.com']
-    start_urls = ['http://http://www.wxapp-union.com/']
+    #  特别注意！　allowed_domains = ['http://www.wxapp-union.com']这样写会爬取不到内容，只写域名就可以了
+    allowed_domains = ['wxapp-union.com']
+    start_urls = ['http://www.wxapp-union.com/portal.php?mod=list&catid=2&page=1']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=r'.+mod=list&catid=2&page=\d'),follow=True),
+        Rule(LinkExtractor(allow=r'.+article-.+\.html'), callback="parse_detail",follow=False),
     )
 
-    def parse_item(self, response):
-        i = {}
-        #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
-        #i['name'] = response.xpath('//div[@id="name"]').extract()
-        #i['description'] = response.xpath('//div[@id="description"]').extract()
-        return i
+    def parse_detail(self, response):
+        title = response.xpath("//h1[@class='ph']/text()").get()
+        author_p = response.xpath("//p[@class='authors']")
+        author = author_p.xpath(".//a/text()").get()
+        pub_time = author_p.xpath(".//span/text()").get()
+        article_content = response.xpath("//td[@id='article_content']//text()").getall()
+        content = "".join(article_content).strip()
+        item = WxappItem(title=title,author=author,pub_time=pub_time,content=content)
+        yield item
